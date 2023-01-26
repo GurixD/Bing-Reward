7775b7101ac4fcd49d622d9413aff1cdb2900e2c
Valentin Wyss <valentin.wyss@hes-so.ch>
2023-01-26 03:30:12 +0100

    Use reqwest

--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,22 @@ use chrono::{Days, NaiveDate, Utc};
 use clap::Parser;
 use notify_rust::Notification;
 use rand::distributions::{Alphanumeric, DistString};
-use sqlite::State;
+use reqwest::{cookie::Jar, Client, Url};
+use sqlite::{State, Statement};
 use std::{
+    error::Error,
     fs::{self, File},
     io::{ErrorKind, Read, Seek, Write},
     path::Path,
-    process::{Child, Command},
     str,
+    sync::Arc,
     time::Duration,
 };
-use thirtyfour::{
-    common::capabilities::firefox::FirefoxPreferences, cookie::time::OffsetDateTime, prelude::*,
-    support::sleep,
-};
-
-struct GeckoDriver {
-    child: Child,
-}
-
-impl GeckoDriver {
-    fn start(gecko_driver: String) -> GeckoDriver {
-        let mut command = Command::new(gecko_driver);
-        let child = command.spawn().expect("Failed to start driver");
-
-        GeckoDriver { child }
-    }
-}
-
-impl Drop for GeckoDriver {
-    fn drop(&mut self) {
-        println!("killing child");
-        self.child.kill().expect("Wasn't running");
-    }
-}
+use tokio::time::sleep;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
 struct Cli {
-    /// Gecko driver path
-    #[arg(long, short)]
-    gecko_driver: String,
-
-    /// Firefox path if not installed in default folder
-    #[arg(long, short)]
-    firefox: Option<String>,
-
     /// Firefox profile folder path to get cookies from (Win + R => "firefox -P" to see profiles then hover to see folder)
     #[arg(long, short)]
     profile: String,
@@ -99,11 +70,9 @@ fn main() {
         return;
     }
 
-    let _gecko_driver = GeckoDriver::start(cli.gecko_driver);
+    let cookies_file = cli.profile + r"\cookies.sqlite";
 
-    let cookies = cli.profile + r"\cookies.sqlite";
-
-    let result = run_requests(cookies, cli.firefox);
+    let result = run_requests(cookies_file);
     match result {
         Ok(_) => {
             Notification::new()
@@ -117,94 +86,77 @@ fn main() {
                 .expect("Failed to write");
         }
         Err(e) => {
+            let error = format!("Bing reward failed: {}", e.to_string());
+
             Notification::new()
                 .summary("Reward bing")
-                .body(&format!("Bing reward failed: {}", e.to_string()))
+                .body(&error)
                 .show()
                 .unwrap();
         }
     };
 }
 
-#[tokio::main]
-async fn run_requests(
-    firefox_cookies: String,
-    firefox_path: Option<String>,
-) -> WebDriverResult<()> {
+#[tokio::main(flavor = "current_thread")]
+async fn run_requests(firefox_cookies: String) -> Result<(), Box<dyn Error>> {
     let edge_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763".to_owned();
     let android_agent =
         "Mozilla/5.0 (Android 11; Mobile; rv:83.0) Gecko/83.0 Firefox/83.0".to_owned();
 
-    let cookies = get_firefox_cookies(firefox_cookies);
+    let cookies = get_firefox_cookies(firefox_cookies)?;
 
-    search_with_user_agent(&firefox_path, &cookies, edge_agent, 40).await?; // 40
-    search_with_user_agent(&firefox_path, &cookies, android_agent, 25).await?; // 25
+    search_with_user_agent(&cookies, &edge_agent, 40).await?; // 40
+    search_with_user_agent(&cookies, &android_agent, 25).await?; // 25
 
     Ok(())
 }
 
 async fn search_with_user_agent(
-    firefox_path: &Option<String>,
-    cookies: &Vec<Cookie<'static>>,
-    user_agent: String,
+    cookies: &String,
+    user_agent: &String,
     request_number: i32,
-) -> WebDriverResult<()> {
-    let mut caps = DesiredCapabilities::firefox();
+) -> Result<(), Box<dyn Error>> {
+    let cookie_store = Jar::default();
+    let url = "https://www.bing.com".parse::<Url>().unwrap();
+    cookie_store.add_cookie_str(cookies, &url);
 
-    if let Some(firefox_path) = firefox_path {
-        caps.add_firefox_option("binary", firefox_path)?;
-    }
+    let cookie_store = Arc::new(cookie_store);
 
-    caps.set_headless()?;
-
-    let mut firefox_preference = FirefoxPreferences::new();
-    firefox_preference.set_user_agent(user_agent)?;
-    caps.set_preferences(firefox_preference)?;
-
-    let driver = WebDriver::new("http://127.0.0.1:4444", caps.clone()).await?;
-
-    driver.goto("https://bing.com").await?;
-    for cookie in cookies {
-        driver.add_cookie(cookie.clone()).await?;
-    }
+    let client = Client::builder()
+        .user_agent(user_agent)
+        .cookie_provider(Arc::clone(&cookie_store))
+        .build()?;
 
     for _ in 0..request_number {
         sleep(Duration::from_secs(1)).await;
         let mut random_url = "https://bing.com/search?q=".to_owned();
         random_url.push_str(&Alphanumeric.sample_string(&mut rand::thread_rng(), 16));
 
-        driver.goto(random_url).await?;
+        let _response = client.get(random_url).send().await?;
     }
-
-    driver.quit().await?;
 
     Ok(())
 }
 
-fn get_firefox_cookies(cookie_file: String) -> Vec<Cookie<'static>> {
+fn get_firefox_cookies(cookie_file: String) -> Result<std::string::String, sqlite::Error> {
     let connection = sqlite::open(cookie_file).expect("db Connection failed");
 
     let query =
-        "SELECT * FROM moz_cookies WHERE (host = '.bing.com' OR host = 'www.bing.com') AND value != '' AND originAttributes = ''";
+        "SELECT * FROM moz_cookies WHERE (host = '.bing.com' OR host = 'www.bing.com') AND value != '' AND originAttributes = '' GROUP BY name;";
     let mut statement = connection.prepare(query).unwrap();
 
     let mut cookies = Vec::new();
     while let Ok(State::Row) = statement.next() {
-        let cookie = Cookie::build(
-            statement.read::<String, _>("name").unwrap(),
-            statement.read::<String, _>("value").unwrap(),
-        )
-        .domain(statement.read::<String, _>("host").unwrap())
-        .expires(
-            OffsetDateTime::from_unix_timestamp(statement.read::<i64, _>("expiry").unwrap())
-                .unwrap(),
-        )
-        .path(statement.read::<String, _>("path").unwrap())
-        .http_only(statement.read::<i64, _>("isHttpOnly").unwrap() != 0)
-        .finish();
+        let pair = retrieve_value(&mut statement)?;
 
-        cookies.push(cookie);
+        cookies.push(pair);
     }
 
-    cookies
+    Ok(cookies.join("; "))
+}
+
+fn retrieve_value(statement: &mut Statement) -> Result<std::string::String, sqlite::Error> {
+    let name = statement.read::<String, _>("name")?;
+    let value = statement.read::<String, _>("value")?;
+    Ok(format!("{name}={value}"))
 }
